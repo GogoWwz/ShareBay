d0c9bb47b5b0593559cfafc5775fdc258d6ab0c7
GogoWwz <wwz_zjut_web@163.com>
2019-02-21 17:30:15 +0800

    typescript练习——class

--- src/tstest/类.ts
+++ src/tstest/类.ts
@@ -1,17 +1,50 @@
+// private、protected、public 和 存取器
 class Person {
     private sex: number
+    protected age: number
     public name: string
-    public constructor(name: string, sex: number) {
+    public constructor(name: string, sex: number, age: number) {
         this.name = name
         this.sex = sex
+        this.age = age
     }
+
 }
 
 class Man extends Person {
-    constructor(name: string) {
-        super(name, 1)
+    constructor(name: string, sex: number, age: number) {
+        super(name, sex, age)
+        console.log(`名字——${this.name}`)
+        console.log(`性别——${this.sex}`)
+        console.log(`年龄——${this.age}`)
+    }
+}
+let p1 = new Man('wu weizhen', 1, 20)
+
+/** 
+ * 抽象类
+ * 1、不允许被实例化
+ * 2、抽象类中的抽象方法必须在子类中实现
+ * 
+ * */ 
+
+abstract class Job {
+    public name
+    public constructor(name: string) {
+        this.name = name
+    }
+    public abstract sayName(): string
+}
+
+// let j = new Job('job')
+
+class Teacher extends Job {
+    public place: string
+    
+    public sayName() {
+        return this.name
     }
 }
 
-let p1 = new Person('wu weizhen', 1)
-console.log(`p1的名字——${p1.name}`)
+let teacher: Teacher = new Teacher('wuweizhen')
+console.log(teacher.sayName())
